a04a1ed3e65a0f63c7693f3829fa2a166a5017e9
Annars30 <annarsenikum@gmail.com>
2025-11-13 23:20:03 +0200

    Update dice_1.gd

--- Scripts/dice_1.gd
+++ Scripts/dice_1.gd
@@ -1,9 +1,38 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _ready() -> void:
-	if Input.is_action_pressed("mouse_left_click"):
-		#AnimatedSprite2D.play()
-		
+var is_mouse_over = false
+var spinning := false
 
+
+
+func do_something():
+	var result := randi_range(0,2)
+	animated_sprite.set_frame_and_progress(result, 0)
+	#do stuff with result
+
+
+func _on_area_2d_mouse_entered() -> void:
+	is_mouse_over = true
+
+
+func _on_area_2d_mouse_exited() -> void:
+	is_mouse_over = false
+
+func _process(delta: float) -> void:
+	if spinning:
+		return
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and is_mouse_over: 
+		animated_sprite.play("Rolling")
+		wait_and_stop()
 	
+	
+func wait_and_stop():
+	spinning = true
+	var wait_time = randf_range(1.1, 2)
+	print(wait_time)
+	await get_tree().create_timer(wait_time).timeout
+	animated_sprite.stop()
+	do_something()
+	spinning = false
