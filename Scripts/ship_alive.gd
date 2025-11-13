3551ca3c13077fc6cb547e1cb77b7a178c4c6d61
Annars30 <annarsenikum@gmail.com>
2025-11-13 23:43:21 +0200

    Update ship_alive.gd

--- Scripts/ship_alive.gd
+++ Scripts/ship_alive.gd
@@ -1,27 +1,24 @@
 extends CharacterBody2D
 
-#THIS WAS BEFORE
+#MY VERSION
 #func _process(delta: float) -> void:
 	#if Input.is_action_pressed("move_right"):
-		#position.x += 7
+		#position.x += 10
 	#if Input.is_action_pressed("move_left"):
-		#position.x -= 7
-		
-	
-const SPEED = 400.0
-var collision = move_and_collide
-
+		#position.x -= 10
 		
 		
+		
+		
+	#PONG VERSION
+const SPEED = 600.0
 func _process(delta: float) -> void:
 	var direction = 0.0
 	if Input.is_action_pressed("move_right"):
 		direction +=1
 	if Input.is_action_pressed("move_left"):
 		direction -=1
-	velocity.x = direction * SPEED
-
-	move_and_slide()
-
-	 
 	
+	velocity.y = 0
+	velocity.x = direction * SPEED
+	move_and_slide()
