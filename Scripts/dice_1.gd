bfa0fd62a1616d760cc7861ca4463f4eeb1e4b8f
Annars30 <annarsenikum@gmail.com>
2025-11-17 20:42:39 +0200

    Update dice_1.gd

--- Scripts/dice_1.gd
+++ Scripts/dice_1.gd
@@ -8,14 +8,13 @@ var spinning := false
 
 
 func do_something():
-	var result := randi_range(0,2)
+	var result := randi_range(0,1)
 	animated_sprite.set_frame_and_progress(result, 0)
 	#do stuff with result
 
 
 func _on_area_2d_mouse_entered() -> void:
 	is_mouse_over = true
-
 
 func _on_area_2d_mouse_exited() -> void:
 	is_mouse_over = false
@@ -26,7 +25,6 @@ func _process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and is_mouse_over: 
 		animated_sprite.play("Dice_1")
 		wait_and_stop()
-	
 	
 func wait_and_stop():
 	spinning = true
