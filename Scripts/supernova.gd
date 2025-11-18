35dd852941737a07c38b6dde83b38e59912c0ac5
Annars30 <annarsenikum@gmail.com>
2025-11-18 18:40:19 +0200

    Update supernova.gd

--- Scripts/supernova.gd
+++ Scripts/supernova.gd
@@ -1,32 +1,32 @@
 extends CharacterBody2D
 
-@onready var animated_sprite_1: AnimatedSprite2D = $AnimatedSprite2D
-@onready var supernova_3_color = $AnimatedSprite2D
-var speed = 3
+@onready var animated_sprite_1: AnimatedSprite2D = $AnimatedSprite2D  #dice with colors
+@onready var supernova = $AnimatedSprite2D   #supernova
+var speed = 3 #speed of expanding supernova
 
 #SUPERNOVA EXPANDING
-#func _physics_process(delta: float) -> void:
-		#position += transform.y * speed * delta
+func _physics_process(delta: float) -> void:
+	position += transform.y * speed * delta
 
 	
 #SUPERNOVA CHANGING COLOR
-func start():
-	_ready()
 	
 func _ready() -> void:
-	supernova_3_color.pause()  # stop animation
-	supernova_3_color.set_frame_and_progress(0, 0.0)
+	supernova.pause()  # stop animation
+	supernova.set_frame_and_progress(0, 0.0)
 
-func _process(delta):
+
+
+func _process(_delta):
 	if animated_sprite_1.frame == 0:
 		change_color_3()
 	elif animated_sprite_1.frame == 1:
 		change_color_6()
 	
 func change_color_3():
-	supernova_3_color.set_frame_and_progress(1, 0.0)
+	supernova.set_frame_and_progress(1, 0.0)
 	
 func change_color_6():
-	supernova_3_color.set_frame_and_progress(2, 0.0)
+	supernova.set_frame_and_progress(2, 0.0)
 
 	
