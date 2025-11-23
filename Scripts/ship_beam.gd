701ffb6c880da071691a9be5565dfd75156f255f
Annars30 <annarsenikum@gmail.com>
2025-11-23 21:56:24 +0200

    Update ship_beam.gd

--- Scripts/ship_beam.gd
+++ Scripts/ship_beam.gd
@@ -2,9 +2,16 @@ extends Area2D
 
 @export var sprite_list: Array[Texture2D]
 @onready var ship_beam: Sprite2D = $Sprite2D
+@export var speed: float = 400 
+var direction: Vector2
 
-#func _ready() -> void:
-	#sprite_2d.texture = sprite_list[2]
+@export var color_id: int = 0
+
+func _physics_process(delta):
+	position += direction * speed * delta
 
 func _ready() -> void:
-	ship_beam.texture = sprite_list[5]
+	ship_beam.texture = sprite_list[color_id]
+	
+#func collision():
+	
