e8dc0517f138bb73148dcc116b4d586e1e70df58
Annars30 <annarsenikum@gmail.com>
2025-11-23 21:56:41 +0200

    Create supernova_wave_6.gd

---
+++ Scripts/supernova_wave_6.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var supernova_wave6 = $Sprite2D
+@export var speed: float = 200
+var direction: Vector2 = Vector2.ZERO
+var move_speed := 10.0
+
+
+func _physics_process(delta):
+	supernova_wave6.position.y += move_speed * delta
