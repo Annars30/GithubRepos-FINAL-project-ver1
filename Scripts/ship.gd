e2f6dd2ddae579ae30ac5088243c1da31b604cdd
Annars30 <annarsenikum@gmail.com>
2025-11-23 21:56:23 +0200

    Update ship.gd

--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 #MY VERSION
 #func _process(delta: float) -> void:
 	#if Input.is_action_pressed("move_right"):
@@ -8,9 +7,7 @@ extends CharacterBody2D
 	#if Input.is_action_pressed("move_left"):
 		#position.x -= 10
 
-		
-		
-	#PONG VERSION
+	#MOVING THE SHIP
 const SPEED = 600.0
 func _process(delta: float) -> void:
 	var direction = 0.0
@@ -18,7 +15,33 @@ func _process(delta: float) -> void:
 		direction +=1
 	if Input.is_action_pressed("move_left"):
 		direction -=1
-	
 	velocity.y = 0
 	velocity.x = direction * SPEED
 	move_and_slide()
+	
+	
+	#SHOOTING
+@onready var muzzle = $Muzzle
+var Beam = preload("res://Scenes/ship_beam.tscn")
+
+func _input(event):
+	if event.is_action_pressed("fire_blue"):
+		shoot(0)
+	if event.is_action_pressed("fire_green"):
+		shoot(1)
+	if event.is_action_pressed("fire_orange"):
+		shoot(2)
+	if event.is_action_pressed("fire_red"):
+		shoot(3)
+	if event.is_action_pressed("fire_violet"):
+		shoot(4)
+	if event.is_action_pressed("fire_yellow"):
+		shoot(5)
+
+func shoot(color_id):
+	var bullet = Beam.instantiate()
+	bullet.global_position = muzzle.global_position
+	bullet.direction = Vector2.UP
+	bullet.color_id = color_id 
+	get_tree().current_scene.add_child(bullet)
+	
