d3ec5c6de74aa0ca7e65f89b28943034192e91e3
Annars30 <annarsenikum@gmail.com>
2025-12-04 13:10:16 +0200

    Update supernova_wave_3.gd

--- Scripts/supernova_wave_3.gd
+++ Scripts/supernova_wave_3.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var speed: float = 200
 var direction: Vector2 = Vector2.ZERO
 var move_speed := 10
+var dice1_result: int 
 
 func _physics_process(delta):
 	supernova_wave3.position.y += move_speed * delta
