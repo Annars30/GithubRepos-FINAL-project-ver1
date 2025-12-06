d05394993b91e3bd92865c39354fa8f55f363932
Annars30 <annarsenikum@gmail.com>
2025-12-06 15:23:07 +0200

    Update supernova_wave.gd

--- Scripts/supernova_wave.gd
+++ Scripts/supernova_wave.gd
@@ -1,8 +1,15 @@
-extends Area2D
+extends CharacterBody2D
 
+@onready var supernova_wave: AnimatedSprite2D = $AnimatedSprite2D
+@export var speed: float = 200
+var direction: Vector2 = Vector2.ZERO
+var move_speed := 10
+var dice1_result: int 
 
-@export var sprite_list: Array[Texture2D]
-@onready var supernova_wave: Sprite2D = $Sprite2D
-
-func _ready() -> void:
-	supernova_wave.texture = sprite_list[0]
+func wave_color():
+	if supernova_wave.sprite_frames.has_animation("wave"):
+		supernova_wave.play("wave")
+		supernova_wave.frame = dice1_result
+		
+func _physics_process(delta):
+	supernova_wave.position.y += move_speed * delta
