8f451ba8dd15cd7a0a51902fd0b69c3b9b9daf2a
Annars30 <annarsenikum@gmail.com>
2025-12-06 15:23:03 +0200

    Update supernova.gd

--- Scripts/supernova.gd
+++ Scripts/supernova.gd
@@ -1,10 +1,9 @@
 extends CharacterBody2D
-class_name Supernova
+
 
 @onready var supernova = $AnimatedSprite2D   #supernova
-@onready var supernova_wave3 = $Sprite2D #supernova wave3
-@onready var supernova_wave6 = $Sprite2D #supernova wave6
-@onready var supernova_beams3 = $Sprite2D #supernova beams3
+@onready var supernova_wave: AnimatedSprite2D = $AnimatedSprite2D#supernova wave
+@onready var supernova_beams = $Sprite2D #supernova beams
 var expand := false
 var expand_speed := 7.0
 @onready var marker_2d: Marker2D = $Marker2D
@@ -12,6 +11,8 @@ var expand_speed := 7.0
 var current_color_id : int
 var beam_colors_0 := [1, 2, 4]
 var beam_colors_1 := [0, 1, 2, 3, 4, 5]
+@onready var beam_timer: Timer = $BeamTimer
+var dice1_result: int
 
 
 #DICE1 ACTION EFFECT (3color,6color)
@@ -29,7 +30,7 @@ func change_color(color_id : int):
 
 func _on_dice_1_rolled(result: int) -> void:
 	change_color(result)
-
+	
 
 
 #DICE2 ACTION EFFECT (0beam,1wave,2expand)
@@ -42,45 +43,44 @@ func _on_dice_2_rolled(result: int) -> void:
 	if result == 2:
 		expand = true
 	elif result == 0:
-		super_beam3_shoot(current_color_id)
+		super_beam_shoot(current_color_id)
+		beam_timer.start()
 	elif result == 1:
-		wave3_move()
-	
-var wave3_scene = preload("res://Scenes/supernova_wave_3.tscn")
-func wave3_move():
-	var wave = wave3_scene.instantiate()
+		wave_move()
+#WAVE
+var wave_scene = preload("res://Scenes/supernova_wave.tscn")
+func wave_move():
+	var wave = wave_scene.instantiate()
 	wave.global_position = Vector2(646, 97)
 	wave.direction = Vector2.DOWN
 	get_tree().current_scene.add_child(wave)
+	wave.wave_color()
 
-var wave6_scene = preload ("res://Scenes/supernova_wave_6.tscn")
-func wave6_move():
-	var wave2 = wave6_scene.instantiate()
-	wave2.global_position = Vector2(646, 97)
-	wave2.direction = Vector2.DOWN
-	get_tree().current_scene.add_child(wave2)
-	
-	
-var super_beam3 = preload ("res://Scenes/supernova_beam.tscn")
-func super_beam3_shoot(color_id):
+#BEAMS
+var super_beam = preload ("res://Scenes/supernova_beam.tscn")
+func super_beam_shoot(color_id):
 	if color_id == 0:
 		for number in beam_colors_0:
-			var supbeam3 = super_beam3.instantiate()
-			supbeam3.direction = Vector2.DOWN
+			var supbeam = super_beam.instantiate()
+			supbeam.direction = Vector2.DOWN
 		
 			var random_x := randi_range(marker_2d.position.x, marker_2d_2.position.x)
-			add_child(supbeam3)
-			supbeam3.set_color(number)
-			supbeam3.position = Vector2(random_x, marker_2d.position.y)
-			print(supbeam3.position)
+			add_child(supbeam)
+			supbeam.set_color(number)
+			supbeam.position = Vector2(random_x, marker_2d.position.y)
+			print(supbeam.position)
 	elif color_id == 1:
 		for number in beam_colors_1:
-			var supbeam3 = super_beam3.instantiate()
-			supbeam3.direction = Vector2.DOWN
+			var supbeam = super_beam.instantiate()
+			supbeam.direction = Vector2.DOWN
 			
 			var random_x := randi_range(marker_2d.position.x, marker_2d_2.position.x)
-			add_child(supbeam3)
-			supbeam3.set_color(number)
-			supbeam3.position = Vector2(random_x, marker_2d.position.y)
-			print(supbeam3.position)
-	
+			add_child(supbeam)
+			supbeam.set_color(number)
+			supbeam.position = Vector2(random_x, marker_2d.position.y)
+			print(supbeam.position)
+
+
+#BEAMS SHOOTING INFINITELY
+func _on_beam_timer_timeout() -> void:
+	super_beam_shoot(current_color_id)
