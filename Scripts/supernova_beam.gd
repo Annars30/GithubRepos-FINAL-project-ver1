0347d3583ba04849d9b45157191a0547453327c2
Annars30 <annarsenikum@gmail.com>
2025-12-06 23:02:37 +0200

    Update supernova_beam.gd

--- Scripts/supernova_beam.gd
+++ Scripts/supernova_beam.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 @export var sprite_list: Array[Texture2D]
 @onready var supernova_beam: Sprite2D = $Sprite2D
-@export var speed: float = 200 
+@export var speed: float = 160 
 var direction: Vector2
 @export var color_id: int
 
