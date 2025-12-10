ec3cf746b7efd9c7a1693790e59268ec31afae79
Annars30 <annarsenikum@gmail.com>
2025-12-10 20:32:55 +0200

    Update ship_beam.gd

--- Scripts/ship_beam.gd
+++ Scripts/ship_beam.gd
@@ -5,11 +5,6 @@ class_name ShipBeam
 @export var speed: float = 350 
 var direction: Vector2
 @export var color_id: int = 0
-@onready var SupernovaStar = $AnimatedSprite2D 
-@onready var marker_2d: Marker2D = $Marker2D
-@onready var marker_2d_2: Marker2D = $Marker2D2
-
-#@export var wave = $AnimatedSprite2D
 
 func _ready() -> void:
 	ship_beam.texture = sprite_list[color_id]
@@ -18,12 +13,29 @@ func _physics_process(delta):
 	position += direction * speed * delta
 
 
+#DESTORY SUPERNOVA COLORED ZONES - COLORED ZONES NOT DONE YET
+#func _on_body_entered(body: Node2D) -> void:
+	#if body is SupernovaStar and SupernovaWave:
+		#var Star = body as SupernovaStar
+		  #var orange_zone = randi_range (int(marker_2d_3.position.x), int(marker_2d_4.position.x))
+		  #var orange_zone = color_id (2)
+		  #if color_id == 0:
+		#body.queue_free()
+		#queue_free()
+		#print ("Collision")
+		  #orange_zone.queue_free()
+		
+#DESTORY SUPERNOVAWAVE COLORED ZONES - COLORED ZONES NOT DONE YET
 #func _on_body_entered(body: Node2D) -> void:
 	#if body is SupernovaWave:
-		#var wave := body as SupernovaWave
-		#var wave_color := wave.get_zone_color()
-		#if color_id == wave_color:
-			#var random_x := randi_range(int(marker_2d.position.x), int(marker_2d_2.position.x))
-			#wave.position.x = random_x
-			#wave.queue_free()
-			#queue_free()
+		#var Wave = body as SupernovaWave
+		  #var orange_zone = randi_range (int(marker_2d_3.position.x), int(marker_2d_4.position.x))
+		  #var orange_zone = color_id (2)
+		  #if color_id == 0:
+		#body.queue_free()
+		#queue_free()
+		#print ("Collision")
+		  #orange_zone.queue_free()
+		
+		
+		
