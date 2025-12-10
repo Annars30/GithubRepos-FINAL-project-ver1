c0a2f5c3dacf5860f9af24e139cb7ccb25ac4a67
Annars30 <annarsenikum@gmail.com>
2025-12-10 20:32:50 +0200

    Update play_button.gd

--- Scripts/play_button.gd
+++ Scripts/play_button.gd
@@ -5,4 +5,4 @@ func _input(event):
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if is_pixel_opaque(get_local_mouse_position()):
 				get_tree().change_scene_to_file("res://Scenes/game.tscn")
-				
+			
