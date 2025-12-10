f711ae33ce638ac284dfb38ccf7250c15fc26d89
Annars30 <annarsenikum@gmail.com>
2025-12-10 20:33:00 +0200

    Update supernova_wave.gd

--- Scripts/supernova_wave.gd
+++ Scripts/supernova_wave.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
-
+class_name SupernovaWave
 @onready var supernova_wave: AnimatedSprite2D = $AnimatedSprite2D
 @export var speed: float = 210
 var direction: Vector2 = Vector2.ZERO
 var move_speed := 10
 var dice1_result: int 
 
+#SETTING THE COLOR OF SUPERVAWES ACCORDING TO DICE1 RESULT
 func wave_color(dice1_result: int):
 	supernova_wave.frame = dice1_result
 
+#SUPERWAVE MOVING
 func _physics_process(delta):
 	supernova_wave.position.y += move_speed * delta
